4aca81df21c9cf7abb1ea23fd5137655cc616dc3
Masoud-web <rezajavan687@gmail.com>
2026-07-24 23:58:49 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,80 @@
+function appendValue(value){
 
-
-// گرفتن عناصر صفحه
-const input = document.getElementById("taskInput");
-const addButton = document.getElementById("addBtn");
-const taskList = document.getElementById("taskList");
-
-
-// اضافه کردن وظیفه جدید
-addButton.addEventListener("click", addTask);
-
-
-// اضافه کردن با دکمه Enter
-input.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        addTask();
-    }
-});
-
-
-// تابع اضافه کردن آیتم
-function addTask() {
-
-    let text = input.value.trim();
-
-    if (text === "") {
-        alert("لطفاً یک متن وارد کنید");
-        return;
-    }
-
-
-    // ساخت آیتم جدید
-    const li = document.createElement("li");
-
-
-    li.innerHTML = `
-        <span class="task-text">${text}</span>
-
-        <div>
-            <button class="done-btn">
-                انجام شد
-            </button>
-
-            <button class="delete">
-                حذف
-            </button>
-        </div>
-    `;
-
-
-    // اضافه کردن به لیست
-    taskList.appendChild(li);
-
-
-    // پاک کردن ورودی
-    input.value = "";
-
-
-    // فعال کردن دکمه انجام شد
-    const doneButton = li.querySelector(".done-btn");
-
-    doneButton.addEventListener("click", function() {
-
-        li.classList.toggle("completed");
-
-    });
-
-
-
-    // فعال کردن دکمه حذف
-    const deleteButton = li.querySelector(".delete");
-
-    deleteButton.addEventListener("click", function() {
-
-        li.remove();
-
-    });
+    document.getElementById("display").value += value;
 
 }
+
+function clearDisplay(){
+
+    document.getElementById("display").value = "";
+
+}
+
+function calculate(){
+
+    let display = document.getElementById("display");
+
+    try{
+
+        display.value = eval(display.value);
+
+    }catch{
+
+        alert("عبارت وارد شده صحیح نیست.");
+
+        display.value = "";
+
+    }
+
+}
+
+// حذف آخرین کاراکتر
+function backspace(){
+
+    let display = document.getElementById("display");
+
+    display.value = display.value.slice(0,-1);
+
+}
+
+// پشتیبانی از صفحه کلید
+document.addEventListener("keydown",function(event){
+
+    let key = event.key;
+
+    if(
+        (key >= "0" && key <= "9") ||
+        key=="+" ||
+        key=="-" ||
+        key=="*" ||
+        key=="/" ||
+        key=="." ||
+        key=="(" ||
+        key==")"
+    ){
+
+        appendValue(key);
+
+    }
+
+    else if(key=="Enter"){
+
+        event.preventDefault();
+
+        calculate();
+
+    }
+
+    else if(key=="Backspace"){
+
+        backspace();
+
+    }
+
+    else if(key=="Delete"){
+
+        clearDisplay();
+
+    }
+
+});
