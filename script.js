aea24e019faf7485835389f6b435cf39b2f91999
Masoud-web <rezajavan687@gmail.com>
2026-07-24 20:10:27 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function hello() {
-    alert("تست موفق");
+    let name = prompt("نام شما چیست؟");
+
+    document.getElementById("message").innerHTML =
+        "سلام " + name + " 🌹";
+}
+
+function darkMode() {
+    document.body.style.background = "black";
+    document.body.style.color = "white";
 }
